3c30f884f9dfdf2c153b6d6f693a9df313c2c9c1
Shaun Gittins <shaungg97@hotmail.com>
2022-01-11 23:18:24 +1100

    Create CompareByLastNameOnly to only compare last name and not given names at all

---
+++ name-sorter/CompareByLastNameOnly.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace name_sorter
+{
+    public class CompareByLastNameOnly : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return new CaseInsensitiveComparer().Compare(x.LastName, y.LastName);
+        }
+    }
+}
